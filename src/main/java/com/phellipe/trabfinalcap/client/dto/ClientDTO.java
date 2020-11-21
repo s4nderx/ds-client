45740b071c825351055d3429888a5c7a126aceab
Phellipe Sander <phellipe.sander1@gmail.com>
2020-11-21 01:08:00 -0300

    ClientDTO, repository, SecurityConfig

--- src/main/java/com/phellipe/trabfinalcap/client/dto/ClientDTO.java
+++ src/main/java/com/phellipe/trabfinalcap/client/dto/ClientDTO.java
@@ -1,44 +1,40 @@
-package com.phellipe.trabfinalcap.client.entities;
+package com.phellipe.trabfinalcap.client.dto;
 
-import javax.persistence.*;
+import com.phellipe.trabfinalcap.client.entities.Client;
+
+import javax.persistence.Column;
 import java.io.Serializable;
 import java.time.Instant;
 
-@Entity
-@Table(name = "tb_client")
-public class Client implements Serializable {
-private static final long serialVersionUID = 1L;
+public class ClientDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String name;
     private String cpf;
     private Double income;
-
-    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant birthDate;
-
     private Integer children;
 
-    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
-    private Instant createdAt;
-
-    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
-    private Instant updatedAt;
-
-    public Client() {
-
+    public ClientDTO() {
     }
 
-    public Client(Long id, String name, String cpf, Double income, Instant birthDate, Integer children) {
+    public ClientDTO(Long id, String name, String cpf, Double income, Instant birthDate, Integer children) {
         this.id = id;
         this.name = name;
         this.cpf = cpf;
         this.income = income;
         this.birthDate = birthDate;
         this.children = children;
+    }
+
+    public ClientDTO(Client entity) {
+        this.id = entity.getId();
+        this.name = entity.getName();
+        this.cpf = entity.getCpf();
+        this.income = entity.getIncome();
+        this.birthDate = entity.getBirthDate();
+        this.children = entity.getChildren();
     }
 
     public Long getId() {
@@ -53,8 +49,8 @@ private static final long serialVersionUID = 1L;
         return name;
     }
 
-    public void setName(String nome) {
-        this.name = nome;
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getCpf() {
@@ -87,23 +83,5 @@ private static final long serialVersionUID = 1L;
 
     public void setChildren(Integer children) {
         this.children = children;
-    }
-
-    public Instant getCreatedAt() {
-        return createdAt;
-    }
-
-    public Instant getUpdatedAt() {
-        return updatedAt;
-    }
-
-    @PrePersist
-    public void PrePersist() {
-        createdAt = Instant.now();
-    }
-
-    @PreUpdate
-    public void PreUpdate() {
-        updatedAt = Instant.now();
     }
 }
