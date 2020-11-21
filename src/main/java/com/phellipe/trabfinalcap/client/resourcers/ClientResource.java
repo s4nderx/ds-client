06824e4b153a2fe918bdff5c1f10072a123251b4
Phellipe Sander <phellipe.sander1@gmail.com>
2020-11-21 11:34:20 -0300

    Client create, update, find paged

--- src/main/java/com/phellipe/trabfinalcap/client/resourcers/ClientResource.java
+++ src/main/java/com/phellipe/trabfinalcap/client/resourcers/ClientResource.java
@@ -4,11 +4,14 @@ import com.phellipe.trabfinalcap.client.dto.ClientDTO;
 import com.phellipe.trabfinalcap.client.entities.Client;
 import com.phellipe.trabfinalcap.client.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/clients")
@@ -17,9 +20,34 @@ public class ClientResource {
     @Autowired
     private ClientService service;
 
+    @PostMapping
+    public ResponseEntity<ClientDTO> create(ClientDTO dto){
+        dto = service.create(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<ClientDTO>> findAllPaged(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+            @RequestParam(value = "direction", defaultValue = "ASC") String direction,
+            @RequestParam(value = "orderBy", defaultValue = "name") String orderBy
+    ){
+        PageRequest pageRequest = PageRequest.of(page,linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+        Page<ClientDTO> list = service.findAllPaged(pageRequest);
+        return ResponseEntity.ok().body(list);
+    }
+
     @GetMapping(value = "/{id}")
     public ResponseEntity<ClientDTO> findById(@PathVariable Long id){
         ClientDTO dto = service.findById(id);
+        return ResponseEntity.ok().body(dto);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ClientDTO> update(@PathVariable Long id, @RequestBody ClientDTO dto){
+        dto = service.update(id, dto);
         return ResponseEntity.ok().body(dto);
     }
 
--- src/main/java/com/phellipe/trabfinalcap/client/services/ClientService.java
+++ src/main/java/com/phellipe/trabfinalcap/client/services/ClientService.java
@@ -4,10 +4,15 @@ import com.phellipe.trabfinalcap.client.dto.ClientDTO;
 import com.phellipe.trabfinalcap.client.entities.Client;
 import com.phellipe.trabfinalcap.client.repositories.ClientRepository;
 import com.phellipe.trabfinalcap.client.services.exceptions.ResourceNotFoundException;
+import jdk.jfr.Category;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.Optional;
 
 @Service
@@ -16,11 +21,47 @@ public class ClientService {
     @Autowired
     ClientRepository repository;
 
-    @Transactional
+    @Transactional(rollbackFor = Exception.class)
+    public ClientDTO create(ClientDTO dto){
+        Client entity = new Client();
+        transformDtoToEntity(dto, entity);
+        entity = repository.save(entity);
+        return new ClientDTO(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ClientDTO> findAllPaged(PageRequest pageRequest) {
+        Page<Client> list = repository.findAll(pageRequest);
+        return list.map(ClientDTO::new);
+    }
+
+    @Transactional(readOnly = true)
     public ClientDTO findById(Long id){
         Optional<Client> obj = repository.findById(id);
         Client entity= obj.orElseThrow(() -> new ResourceNotFoundException("Client not found"));
         return new ClientDTO(entity);
     }
+
+    @Transactional(rollbackFor = Exception.class)
+    public ClientDTO update(Long id, ClientDTO dto) {
+        try {
+            Client entity = repository.getOne(id);
+            transformDtoToEntity(dto, entity);
+            entity = repository.save(entity);
+            return new ClientDTO(entity);
+        }catch (EntityNotFoundException ex){
+            throw new ResourceNotFoundException("Id not found " + id);
+        }
+    }
+
+    private void transformDtoToEntity(ClientDTO dto, Client entity) {
+        entity.setName(dto.getName());
+        entity.setBirthDate(dto.getBirthDate());
+        entity.setCpf(dto.getCpf());
+        entity.setChildren(dto.getChildren());
+        entity.setIncome(dto.getIncome());
+    }
+
+
 
 }
